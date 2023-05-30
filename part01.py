71c2620a7b5c9fc28bdfb4366009cc932d598126
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-05-30 11:05:42 +0100

    adding JS Python comparison notes

--- part01.py
+++ part01.py
@@ -11,14 +11,23 @@ print("I live at 123 Fake Street, London, UK.")
 print("My interests include coding, football, and astronomy.")
 print(f"The sum of 1 and 1 is: {1+1}")
 
-# No task 3
+# There was no task 2
 # Task 3: using input function
 # complete the rest of the code
 # Use a variable and invoke the input function to ask the user for a second number
 # add the two numbers
 # print out the total of the two numbers
 
+# JS Python comparison
+# JS uses window.prompt() to get user input, Python uses input
 first_number = int(input("Enter first number: "))
 second_number = int(input("Enter second number: "))
 total = first_number + second_number
 print(f"The sum of the two numbers is: {total}.")
+
+# JS Python comparison
+# JS has array type, Python has more types of array (3) than JS 
+# (1) list: ordered
+# (2) tuples: ordered, cannot be changed (and are thus faster than lists)
+# (3) set: unordered: list of unique items
+# JS has object type whereas Python has dictionary type (Python keys must be string or number)
