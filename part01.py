652c2761a888e1270991431e74b5696526347802
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-05-30 10:47:00 +0100

    first part done

---
+++ part01.py
@@ -0,0 +1,24 @@
+# Task 1
+# Exercise: use the print statement to print
+# Your name
+# Your address
+# Your interests
+# Perform either addition or calculation with two numbers
+# Use text in your addition
+
+print("My name is Stewart.")
+print("I live at 123 Fake Street, London, UK.")
+print("My interests include coding, football, and astronomy.")
+print(f"The sum of 1 and 1 is: {1+1}")
+
+# No task 3
+# Task 3: using input function
+# complete the rest of the code
+# Use a variable and invoke the input function to ask the user for a second number
+# add the two numbers
+# print out the total of the two numbers
+
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+total = first_number + second_number
+print(f"The sum of the two numbers is: {total}.")
